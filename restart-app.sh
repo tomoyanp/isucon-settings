9dd4af6cb5c92c7ae8f3d0f02c777f118e7cc28d
tomoyanp <tomoyanpy@gmail.com>
2022-02-06 01:40:54 +0000

    m

--- restart-app.sh
+++ restart-app.sh
@@ -1,4 +1,4 @@
 cd ~/isucari/webapp/go && make
 sudo systemctl daemon-reload
-sudo systemctl restart isucari.golang.service
+sudo systemctl restart isubnb.go.service
 sudo systemctl restart nginx.service
